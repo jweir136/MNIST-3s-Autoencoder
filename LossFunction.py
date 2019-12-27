59ffd5f055635e4fb3ee8eaf040263275de35988
jweir136 <jweir136@yahoo.com>
2019-12-26 20:02:51 -0500

    fixed the KL Loss Function

--- LossFunction.py
+++ LossFunction.py
@@ -4,9 +4,9 @@ import torch.nn.functional as fn
 
 def loss_function(pred_x, x, mu, logvar, epoch):
   mse = fn.mse_loss(pred_x, x)
-  kld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - torch.exp(logvar))
+  kld = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
   
   if epoch >= 25:
     return mse + kld
 
-  return mse
+  return mse + kld
